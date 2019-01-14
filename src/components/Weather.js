603fa9632d9ab5fbac8165255808a01584cbf41c
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-14 15:43:50 -0700

    Finished components and App without styles

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,9 +5,10 @@ class Weather extends React.Component {
         return (
             <div>
             { this.props.city && this.props.country && <p>Location: { this.props.city } { this.props.country }</p>}
-            { this.props.city && <p>Temperature: { this.props.temperature }</p>}
-            { this.props.humidity && <p>Humidity: { this.props.humidity}</p>}
+            { this.props.city && <p>Temperature: { this.props.temperature } F</p>}
+            { this.props.humidity && <p>Humidity: { this.props.humidity} % </p>}
             { this.props.description && <p>Conditions: { this.props.description}</p>}
+            { this.props.error && <p>{ this.props.error}</p>}
             </div>
         );
     }
--- src/components/app.js
+++ src/components/app.js
@@ -21,16 +21,27 @@ export default class App extends Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      errror:""
-    })
-  }
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        errror:""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined, 
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter City and Country."
+      });
+    }
+  }  
   render() {
     return (
       <div className='app'>
