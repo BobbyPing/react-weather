9e5cc179318891aff2c9bf0384084b4d09059f1f
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-14 13:30:14 -0700

    Added Necessary Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import Titles from "./Titles"; 
+import Form from "./Form";
+import Weather from "./Weather";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+        <div className = "header">
+          <Titles />
+          <Form />
+          <Weather />
+        </div>
       </div>
     );
   }
