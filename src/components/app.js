1ffc4e5140d666992358ee0fe939f639420124b2
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-14 15:19:48 -0700

    built out weather component

--- src/components/app.js
+++ src/components/app.js
@@ -19,7 +19,7 @@ export default class App extends Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
     this.setState({
@@ -28,7 +28,7 @@ export default class App extends Component {
       country: data.sys.country,
       humidity: data.main.humidity,
       description: data.weather[0].description,
-      errror:"";
+      errror:""
     })
   }
   render() {
@@ -37,7 +37,13 @@ export default class App extends Component {
         <div className = "header">
           <Titles />
           <Form getWeather={this.getWeather}/>
-          <Weather />
+          <Weather temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+          />
         </div>
       </div>
     );
