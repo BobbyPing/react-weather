f17959bb3a1fcf310e7f0b880517f7358960d102
Bobby <Bobby@Bobbys-Creation-Station.local>
2019-01-15 14:32:58 -0700

    Final commit

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,12 +6,13 @@ import image1 from './001.jpg'
 import image2 from './002.jpg'
 import image3 from './003.jpg'
 import image4 from './004.jpg'
+import image5 from './005.jpg'
 
 export default class Image extends Component {
   render () {
     return (
       <div>
-        <BackgroundSlideshow images={[ image1, image2, image3, image4 ]} />
+        <BackgroundSlideshow images={[ image1, image2, image3, image4, image5 ]} />
       </div>
     )
   }
